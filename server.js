e92fb03cb4cabbdd520888ebb14a1e25b717cd3a
Brendan Heaney <47929389+BrendanJeaney@users.noreply.github.com>
2019-12-06 06:23:02 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ var fs = require('fs');
 const express = require('express');
 const pug = require('pug');
 var app = express();
+const pgp = require('pg-promise')();
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json());              
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+const dbConfig = {
+	host: 'localhost',
+	port: 5432,
+	database: 'postgres',
+	user: 'postgres',
+	password: ''
+};
+
+let db = pgp(dbConfig);
 
 app.set("view engine", "pug");
 
@@ -38,6 +52,56 @@ app.get('/signup', function(req,res){
   res.render('signup', {
     title: 'signup page',
   });
+});
+
+
+app.post('/signup', function(req, res)
+{
+  var user = req.body.uname;
+  var pword = req.body.passwrd;
+  // console.log(user, pword)
+  // console.log("INSERT INTO users(username, pwd) VALUES('" + user + "', '" +  pword + ");")
+  var insert_data = "INSERT INTO users(username, pwd) VALUES('" + user + "', '" +  pword + "');";
+  console.log(user, pword)
+  //var query2 = "SELECT COUNT(*) FROM users WHERE user = username;";
+
+  db.any(insert_data)
+    .then(info =>
+    {
+      //console.log(info)
+      res.render('signup', {
+        title: 'signup page',
+        data: info
+      })
+      // res.render('homePage', {
+      //   title: 'home page'
+      // })
+    })
+    // .then(() => res.render('homePage'))
+    .catch(function(err){
+        console.log("Error: ", err);
+    })
+});
+
+
+app.post('/homePage/login', function(req, res)
+{
+  var usser = req.body.username;
+  var pass = req.body.paswerd;
+  var query1 = "SELECT * FROM users WHERE username = '" + usser + "' AND pwd = '" + pass + "';";
+  db.any(query1)
+  .then(info => {
+    res.render('homepage', {
+      title: "homePage",
+      data: info
+    })
+    res.send('alert("Success")')
+  })
+  .catch(function(err)
+  {
+    console.log("Error: ", err);
+  })
+
 });
 
 app.listen(3000);
