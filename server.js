67aa36b794845c3933093d167012fa2e3a1ca18e
samsullivan878 <46061461+samsullivan878@users.noreply.github.com>
2019-12-06 01:00:40 -0700

    server core

---
+++ server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+const express = require('express');
+const pug = require('pug');
+var app = express();
+
+app.set("view engine", "pug");
+
+app.get('/homePage', function(req, res){
+  res.render('homePage', {
+    title: 'home page',
+  });
+});
+
+app.get('/videoPage', function(req, res){
+  res.render('videoPage', {
+    title: 'video page',
+  });
+});
+
+app.get('/popMemes', function(req, res){
+  res.render('popMemes', {
+    title: 'Pop Memes',
+  });
+});
+
+app.get('/uploadPage', function(req, res){
+  res.render('uploadPage', {
+    title: 'upload page',
+  });
+});
+
+app.listen(3000);
