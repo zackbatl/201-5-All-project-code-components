20ad3cca193e303d11430841a8c54122ab391ada
samsullivan878 <46061461+samsullivan878@users.noreply.github.com>
2019-12-16 18:14:36 -0700

    merged signup

--- testserver.js
+++ testserver.js
@@ -92,6 +92,56 @@ app.get('/signup', function(req,res){
   });
 });
 
+app.post('/signup', function(req, res)
+{
+  var user = req.body.uname;
+  var pword = req.body.passwrd;
+  // console.log(user, pword)
+  // console.log("INSERT INTO users(username, pwd) VALUES('" + user + "', '" +  pword + ");")
+  var insert_data = "INSERT INTO users(username, pwd) VALUES('" + user + "', '" +  pword + "');";
+  console.log(user, pword)
+  //var query2 = "SELECT COUNT(*) FROM users WHERE user = username;";
+
+  db.any(insert_data)
+    .then(info =>
+    {
+      //console.log(info)
+      res.render('signup', {
+        title: 'signup page',
+        data: info
+      })
+      // res.render('homePage', {
+      //   title: 'home page'
+      // })
+    })
+    // .then(() => res.render('homePage'))
+    .catch(function(err){
+        console.log("Error: ", err);
+    })
+});
+
+
+app.post('/homePage/login', function(req, res)
+{
+  var usser = req.body.username;
+  var pass = req.body.paswerd;
+  var query1 = "SELECT * FROM users WHERE username = '" + usser + "' AND pwd = '" + pass + "';";
+  db.any(query1)
+  .then(info => {
+    res.render('homepage', {
+      title: "homePage",
+      data: info,
+      // command: 'alert("Success")',
+      username: usser
+    })
+    console.log(info)
+  })
+  .catch(function(err)
+  {
+    console.log("Error: ", err);
+  })
+
+});
 
 function find_popular(){ // returns array of image_ids of top ten in like count
   var img_ids = [];
